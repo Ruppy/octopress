b09a1b5cb2b0bb3931b866b89d87324381a87d24
Leonardo Baptista <leonardoobaptistaa@gmail.com>
2014-03-21 17:22:49 -0300

    Fix multi language to use new version

--- source/javascripts/libs/multilanguage.js
+++ source/javascripts/libs/multilanguage.js
@@ -1,9 +1,11 @@
 function showEnglish() {
+  document.cookie="language=#en; expires=Thu, 18 Dec 2999 12:00:00 GMT";
   $('.english').show();
   $('.portuguese').hide();
 }
 
 function showPortuguese() {
+  document.cookie="language=#pt; expires=Thu, 18 Dec 2999 12:00:00 GMT";
   $('.english').hide();
   $('.portuguese').show();
 }
@@ -19,7 +21,31 @@ $( document ).ready(function() {
 
   if(window.location.hash == "#pt") {
     showPortuguese();
-  } else {
+  }
+  else if(window.location.hash == "#en") {
     showEnglish();
   }
+  else {
+    if (getCookie("language") == "#pt") {
+      window.location.hash = "#pt"
+      showPortuguese();
+    }
+    else {
+      window.location.hash = "#en"
+      showEnglish();
+    }
+
+  }
 });
+
+function getCookie(cname)
+{
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i<ca.length; i++)
+    {
+    var c = ca[i].trim();
+    if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+    }
+  return "";
+}
